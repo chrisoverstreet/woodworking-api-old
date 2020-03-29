b11178d9e2cf31a1d6fc83ff5a6849c2098c7a23
Chris Overstreet <chriso@chriso.io>
2020-03-29 11:39:37 -0400

    Update Travis CI

--- src/routes/test/test.routes.ts
+++ src/routes/test/test.routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  return res.send('Test!!!');
+  return res.send('Test!!!!!');
 });
 
 export default router;
